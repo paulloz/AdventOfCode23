4923c280e966d43fbfeb09306a943cf8744db4bb
Paul Joannon <paul.joannon@homoludens.games>
2023-12-13 19:28:51 +0100

    :christmas_tree: Day 13

---
+++ Src/Day13/Day13.fs
@@ -0,0 +1,36 @@
+module AdventOfCode23.Day13
+
+let findReflection (transpose: bool) (pattern: string list) =
+    let pattern = pattern |> List.map List.ofSeq
+    let pattern = if transpose then List.transpose pattern else pattern
+
+    pattern
+    |> List.mapi (fun i line -> i, line)
+    |> List.choose (fun (i, line) ->
+        let n = min i (pattern.Length - i)
+
+        if i >= pattern.Length || n <= 0 then
+            None
+        else
+            let a = pattern |> List.skip (i - n) |> List.truncate n
+            let b = pattern |> List.skip i |> List.truncate n |> List.rev
+            if a = b then Some(i) else None)
+
+let findReflections (pattern: string list) =
+    findReflection true pattern, findReflection false pattern
+
+let main (lines: string list) =
+    let patterns =
+        (lines |> String.concat "|").Split("||")
+        |> Seq.map (fun pattern -> pattern.Split("|") |> List.ofSeq)
+        |> List.ofSeq
+
+    let reflectionValues =
+        patterns
+        |> List.map findReflections
+        |> List.map (fun (h, v) ->
+            let h = h |> List.sum
+            let v = v |> List.map ((*) 100) |> List.sum
+            h + v)
+
+    printfn "%d" (reflectionValues |> List.sum)
